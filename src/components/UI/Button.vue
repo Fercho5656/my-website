051b38ce599bd49e4c758136278b3ea813ea5a38
Fercho5656 <morenomoranfernando@gmail.com>
2023-01-07 23:44:06 -0600

    Add button component

---
+++ src/components/UI/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button
+    class="text-white focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 transition-colors">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+// bg-yellow-400 hover:bg-yellow-500 focus:ring-yellow-300 dark:focus:ring-yellow-900
+
+</script>
