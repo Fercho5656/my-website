9fdcfb81f90183dad096ea9a12c2f08412791b76
Fercho5656 <morenomoranfernando@gmail.com>
2023-01-09 23:23:20 -0600

    Add desktop navbar component

---
+++ src/components/TheNavbar/Desktop/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="flex-1 max-w-xl">
+    <ul class="flex gap-x-4 justify-evenly transition-all text-xl lg:text-2xl lg:font-semibold whitespace-nowrap">
+      <Link href="#home">Home</Link>
+      <Link href="#about-me">About Me</Link>
+      <Link href="#portfolio">Portfolio</Link>
+      <Link href="#blog">Blog</Link>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import Link from './Link.vue'
+</script>
+
+<style scoped>
+
+</style>
