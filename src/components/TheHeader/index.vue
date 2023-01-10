2189b9ff6b7e8ab940e85af97b7409d4395212cd
Fercho5656 <morenomoranfernando@gmail.com>
2023-01-09 23:23:42 -0600

    Improve responsiveness

--- src/components/TheHeader/index.vue
+++ src/components/TheHeader/index.vue
@@ -1,13 +1,14 @@
 <template>
-  <header class="flex items-center justify-between pt-2">
-    <button>
+  <header class="flex items-center justify-evenly pt-3 sm:p-3">
+    <button class="sm:hidden">
       <bars3 class="w-10 aspect-square" />
     </button>
-    <p class="text-3xl text-green-pea-400 font-mono font-semibold">Fercho</p>
+    <p class="text-3xl lg:text-4xl transition-all text-green-pea-400 font-mono font-semibold">Fercho</p>
+    <TheNavbarDesktop class="hidden sm:block" />
     <Button
-      class="flex items-center gap-x-1 text-base bg-green-pea-400 hover:bg-green-pea-500 focus:ring-green-pea-300 dark:focus:ring-green-pea-900">
+      class="flex items-center gap-x-1 text-base lg:text-xl transition-all bg-green-pea-400 hover:bg-green-pea-500 focus:ring-green-pea-300 dark:focus:ring-green-pea-900">
       <Phone class="w-4 aspect-square" />
-      Contáctame
+      Contact Me!
     </Button>
   </header>
 </template>
@@ -16,6 +17,8 @@
 import Bars3 from '../Icons/Bars3.vue'
 import Phone from '../Icons/Phone.vue'
 import Button from '../UI/Button.vue'
+import TheNavbarDesktop from '../TheNavbar/Desktop/index.vue'
+
 </script>
 
 <style scoped>
