60a76ad8e870388ca2e5beb4dbe2c678a2b7d322
Fercho5656 <morenomoranfernando@gmail.com>
2023-01-09 23:23:03 -0600

    Refactor anchor component for navbar

---
+++ src/components/TheNavbar/Desktop/Link.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <a :href="props.href">
+      <slot />
+    </a>
+  </li>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  href: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  href: '#'
+})
+</script>
+
+<style scoped>
+
+</style>
